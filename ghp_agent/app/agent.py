139fd69adaa2592834219028dd413c0d05859a7d
Rohit Naidu <rohitnaidu@google.com>
2025-03-13 12:46:35 +0530

    merge old code

--- ghp_agent/app/agent.py
+++ ghp_agent/app/agent.py
@@ -52,16 +52,6 @@ llm = ChatVertexAI(
     temperature=0,
     max_tokens=1024,
     streaming=True,
-    tools=[search],
-)
-
-# 2. Set up the language model
-llm = ChatVertexAI(
-    model=LLM,
-    location=LOCATION,
-    temperature=0,
-    max_tokens=1024,
-    streaming=True,
     system_instruction=prompts.initial_routing_prompt(),
 ).bind_tools(tools)
 
